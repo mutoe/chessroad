911abc12682e181f8ec5d89689f2854bfe1d3c42
mutoe <mutoe@foxmail.com>
2020-06-20 00:30:37 +0800

    chore: add color constants

---
+++ lib/common/color-constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const LogoColor = Color(0xff6d000d);
+
+  static const Primary = Color(0xff461220);
+  static const Secondary = Color(0x99461220);
+
+  static const DarkBackground = Colors.brown;
+  static const LightBackground = Color(0xffeee0cb);
+
+  static const BoardBackground = Color(0xffebc38d);
+  static const BoardLine = Color(0x996d000d);
+  static const BoardTips = Color(0x666d000d);
+  static const LightLine = Color(0x336d000d);
+
+  static const DarkTextPrimary = Colors.white;
+  static const DarkTextSecondary = Color(0x99ffffff);
+
+  static const FocusPosition = Color(0x99ffffff);
+  static const BlurPosition = Color(0x99ffffff);
+
+  static const BlackPieceColor = Color.fromARGB(0xff, 0x00, 0x00, 0x00);
+  static const BlackPieceBoarderColor = Color.fromARGB(0xff, 0x22, 0x22, 0x22);
+
+  static const RedPieceColor = Color.fromARGB(0xff, 0x7b, 0x00, 0x00);
+  static const RedPieceBorderColor = Color.fromARGB(0xff, 0x66, 0x00, 0x00);
+
+  static const PieceTextColor = Color.fromARGB(0xcc, 0xff, 0xff, 0xff);
+}
